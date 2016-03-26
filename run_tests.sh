03daf408f6fcf0e118a68b06ebbea53b3f4705ad
diskerror <reidw@rawsound.com>
2016-03-25 17:37:05 -0700

    added SingletonTest

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# vendor/bin/phpunit --process-isolation --bootstrap vendor/autoload.php tests
 vendor/bin/phpunit --bootstrap vendor/autoload.php tests
---
+++ tests/SingletonTest.php
@@ -0,0 +1,27 @@
+<?php
+class SingletonTest extends PHPUnit_Framework_TestCase
+{
+	public function testConstruct()
+	{
+		$reflection = new \ReflectionClass('SingletonInstance');
+		$this->assertFalse( $reflection->getConstructor()->isPublic() );
+	}
+
+	public function testClone()
+	{
+		$reflection = new \ReflectionClass('SingletonInstance');
+		$this->assertFalse( $reflection->isCloneable() );
+	}
+
+	public function testExistance()
+	{
+		$s = SingletonInstance::getInstance();
+		$this->assertEquals( $s, Diskerror\Utilities\Registry::get('SingletonInstance') );
+	}
+
+}
+
+class SingletonInstance extends Diskerror\Utilities\Singleton
+{
+	public $someVar = 'and text';
+}
